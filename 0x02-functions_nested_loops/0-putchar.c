c269f2930ea35e66364925ac85f8639ce6238f52
Abd-Mumin <ajiboyemumin99@gmail.com>
2022-06-08 06:38:47 -0700

    This

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,11 +5,11 @@
  * Return: 0
  */
 
-int main(void){
+int main(void) {
 	char x[] = "_putchar";
 	int i;
 
-	for (i = 0; i < 8; i++){
+	for (i = 0; i < 8; i++) {
 		_putchar(x[i]);
 	}
 		_putchar('\n');
